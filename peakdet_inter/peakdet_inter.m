a696c51ddcc84b21ff4dd2eba8ff8a03be57923a
alexis-michaud <michaud.cnrs@gmail.com>
2019-01-16 20:45:13 +0100

    closing display figures after processing 1 item
    Avoids having 4th figure remaining open unnecessarily while script runs,
    and having lots of windows remaining open after script terminates.

--- peakdet_inter/peakdet_inter.m
+++ peakdet_inter/peakdet_inter.m
@@ -474,8 +474,12 @@ for i = 1:maxnb
                         % clearing previous results
                         clear datafile
 
-                        %%%%%%%%%%%%%% running main analysis programme
-                        [f0,Oq,Oqval,DEOPA,goodperiods,OqS,OqvalS,DEOPAS,goodperiodsS,simppeak,SIG,dSIG,SdSIG] = FO(COEF,pathEGG,EGGfilename,time,method,propthresh,resampC,maxF)                    
+                        %%%%%%%%%%%%%% running main analysis programme again
+                        [f0,Oq,Oqval,DEOPA,goodperiods,OqS,OqvalS,DEOPAS,goodperiodsS,simppeak,dSIG,SdSIG] = ...
+    FO(COEF,method,propthresh,resampC,maxF,SIGpart,FS);
+
+% previously:                        [f0,Oq,Oqval,DEOPA,goodperiods,OqS,OqvalS,DEOPAS,goodperiodsS,simppeak,SIG,dSIG,SdSIG] = FO(COEF,pathEGG,EGGfilename,time,method,propthresh,resampC,maxF)      
+
                         % setting a counter for number of periods placed in results
                         % matrix <datafile>
                         chosen = 0;
@@ -496,8 +500,12 @@ for i = 1:maxnb
                         % changing threshold
                         COEF(4) = coefchange2;
 
-                        %%%%%%%%%%%%%% running main analysis programme
-                        [f0,Oq,Oqval,DEOPA,goodperiods,OqS,OqvalS,DEOPAS,goodperiodsS,simppeak,SIG,dSIG,SdSIG] = FO(COEF,pathEGG,EGGfilename,time,method,propthresh,resampC,maxF)                    
+                        %%%%%%%%%%%%%% running main analysis programme again
+                        [f0,Oq,Oqval,DEOPA,goodperiods,OqS,OqvalS,DEOPAS,goodperiodsS,simppeak,dSIG,SdSIG] = ...
+    FO(COEF,method,propthresh,resampC,maxF,SIGpart,FS);
+
+% previously:                        [f0,Oq,Oqval,DEOPA,goodperiods,OqS,OqvalS,DEOPAS,goodperiodsS,simppeak,SIG,dSIG,SdSIG] = FO(COEF,pathEGG,EGGfilename,time,method,propthresh,resampC,maxF)            
+
                         % assigning complementary results in file
                         for ii = 1:length(f0)
                             if simppeak(ii,1) > (bound/FS)
@@ -516,13 +524,57 @@ for i = 1:maxnb
                         % plotting the results
                         figure(1)
                         clf
-                        plot(datafile(:,7),'-pb')
+%                         plot(datafile(:,7),'-pb')
+%                         hold on
+%                         plot(datafile(:,5),'*g')
+%                         plot(datafile(:,8),'or')
+%                         plot(datafile(:,9),'sk')
+%                         % plotting f0
+%                         plot(datafile(:,3),'-pb')
+                        h = axes;
+                        set(h,'Fontsize',12)
+
+                        % Open quotient: when they match, the 4 data points will be
+                        % on top of one another. For maximum visibility: 
+                        % (i) the filled boxes plotted last should
+                        % be smaller, and 
+                        % (ii) the stars (pentagrams) should be plotted after the
+                        % filled boxes.
+
+                        % First lines: Oq values based on barycentre method.
+                        % Values based on smoothed signal: shown as filled light-blue squares.
+                        plot(OqvalS, 'LineStyle','--', 'Marker', 's','Color', [0.1490 0.7686 0.9255], 'MarkerSize',11, 'MarkerFaceColor', [0.1490 0.7686 0.9255])
+                         % Previously, those were shown as black squares:           plot(OqvalS,'sk')
+
+                        % ‘Hold on’: to avoid deletion of 1st plot at later plots. 
                         hold on
-                        plot(datafile(:,5),'*g')
-                        plot(datafile(:,8),'or')
-                        plot(datafile(:,9),'sk')
-                        % plotting f0
-                        plot(datafile(:,3),'-pb')
+
+                        % Oq values based on barycentre method, unsmoothed signal: shown as dark blue pentagrams
+                        plot(Oqval, 'LineStyle','none', 'Marker', 'p','Color', [0 0.0078 0.8824], 'MarkerSize',8, 'MarkerFaceColor', [0 0.0078 0.8824])
+
+                        % Plot boxes for Oq values obtained from smoothed signal. Orange filled
+                        % boxes. (Tried light green for the filled boxes, but that colour did not stand out
+                        % clearly from the blue colour.)
+                        % Colour later changed to lighter orange/yellow, for better contrast with the red stars.
+                        plot(OqS, 'LineStyle','--', 'Marker', 's','Color', [1 .7961 .3765], 'MarkerSize',9, 'MarkerFaceColor', [1 .7961 .3765])
+
+                        % On top of this: plot the values from unsmoothed signal, as filled stars
+                        % (pentagram) without connecting lines. Colour: a reddish Coral colour, to
+                        % stand out better. Values: RGB 231 62 1 = [0.9059 0.2431 0.0039]
+                        plot(Oq, 'LineStyle','none', 'Marker', 'p','Color', 'red', 'MarkerSize',8, 'MarkerFaceColor', 'red')
+
+                        % plotting f0. Colour: light green [0.0196 0.9686 0.6118],
+                        % later changed to deeper green [.0863 .7216 .3059] with
+                        % darker edges [0.12 0.63 0.33], then to green with a solid
+                        % line.
+                        % An idea for improvement: change the colour based on how close to creak it gets. 
+                        % There would be distinct scales for men's voices & women's voices, as appropriate.
+                        % For instance: one colour for the range below 30 Hz, another for the range from 30 to 60, another for 60 to 90, then another for above 90.
+                          %          plot(f0,'LineStyle','--','Marker', 'd','Color',[0.9373    0.6078    0.0588])
+                        plot(f0, 'LineStyle','-', 'LineWidth', 1.5, 'Marker', 'o','Color', [.0863 .7216 .3059], 'MarkerSize',10, 'MarkerFaceColor', [.0863 .7216 .3059])
+
+                        xlabel('Orange: local max, blue: barycentre. Boxes: smoothed dEGG, stars: unsmoothed.')            
+                        ylabel('Green: f_0 in Hz; blue and orange: O_q in %')
                         
                         % setting the <corr> variable so the user can check the results
                         corr = 1;
@@ -555,13 +607,58 @@ for i = 1:maxnb
                     % plotting the results
                     figure(1)
                     clf
-                    plot(datafile(:,7),'-pb')
+%                     plot(datafile(:,7),'-pb')
+%                     hold on
+%                     plot(datafile(:,5),'*g')
+%                     plot(datafile(:,8),'or')
+%                     plot(datafile(:,9),'sk')
+%                     % plotting f0
+%                     plot(datafile(:,3),'-pb')
+                    
+                    h = axes;
+                    set(h,'Fontsize',12)
+
+                    % Open quotient: when they match, the 4 data points will be
+                    % on top of one another. For maximum visibility: 
+                    % (i) the filled boxes plotted last should
+                    % be smaller, and 
+                    % (ii) the stars (pentagrams) should be plotted after the
+                    % filled boxes.
+
+                    % First lines: Oq values based on barycentre method.
+                    % Values based on smoothed signal: shown as filled light-blue squares.
+                    plot(OqvalS, 'LineStyle','--', 'Marker', 's','Color', [0.1490 0.7686 0.9255], 'MarkerSize',11, 'MarkerFaceColor', [0.1490 0.7686 0.9255])
+                     % Previously, those were shown as black squares:           plot(OqvalS,'sk')
+
+                    % ‘Hold on’: to avoid deletion of 1st plot at later plots. 
                     hold on
-                    plot(datafile(:,5),'*g')
-                    plot(datafile(:,8),'or')
-                    plot(datafile(:,9),'sk')
-                    % plotting f0
-                    plot(datafile(:,3),'-pb')
+
+                    % Oq values based on barycentre method, unsmoothed signal: shown as dark blue pentagrams
+                    plot(Oqval, 'LineStyle','none', 'Marker', 'p','Color', [0 0.0078 0.8824], 'MarkerSize',8, 'MarkerFaceColor', [0 0.0078 0.8824])
+
+                    % Plot boxes for Oq values obtained from smoothed signal. Orange filled
+                    % boxes. (Tried light green for the filled boxes, but that colour did not stand out
+                    % clearly from the blue colour.)
+                    % Colour later changed to lighter orange/yellow, for better contrast with the red stars.
+                    plot(OqS, 'LineStyle','--', 'Marker', 's','Color', [1 .7961 .3765], 'MarkerSize',9, 'MarkerFaceColor', [1 .7961 .3765])
+
+                    % On top of this: plot the values from unsmoothed signal, as filled stars
+                    % (pentagram) without connecting lines. Colour: a reddish Coral colour, to
+                    % stand out better. Values: RGB 231 62 1 = [0.9059 0.2431 0.0039]
+                    plot(Oq, 'LineStyle','none', 'Marker', 'p','Color', 'red', 'MarkerSize',8, 'MarkerFaceColor', 'red')
+
+                    % plotting f0. Colour: light green [0.0196 0.9686 0.6118],
+                    % later changed to deeper green [.0863 .7216 .3059] with
+                    % darker edges [0.12 0.63 0.33], then to green with a solid
+                    % line.
+                    % An idea for improvement: change the colour based on how close to creak it gets. 
+                    % There would be distinct scales for men's voices & women's voices, as appropriate.
+                    % For instance: one colour for the range below 30 Hz, another for the range from 30 to 60, another for 60 to 90, then another for above 90.
+                      %          plot(f0,'LineStyle','--','Marker', 'd','Color',[0.9373    0.6078    0.0588])
+                    plot(f0, 'LineStyle','-', 'LineWidth', 1.5, 'Marker', 'o','Color', [.0863 .7216 .3059], 'MarkerSize',10, 'MarkerFaceColor', [.0863 .7216 .3059])
+
+                    xlabel('Orange: local max, blue: barycentre. Boxes: smoothed dEGG, stars: unsmoothed.')            
+                    ylabel('Green: f_0 in Hz; blue and orange: O_q in %')
                 end
                 corr = 1;
             end
@@ -584,7 +681,9 @@ for i = 1:maxnb
                     datafile(cornb,3) = newvalue;
                     figure(4)
                     clf
-                    plot(nonzeros(datafile(:,1)),nonzeros(datafile(:,3)), 'LineStyle','-', 'LineWidth', 1.5, 'Marker', 'o','Color', [.0863 .7216 .3059], 'MarkerSize',10, 'MarkerFaceColor', [.0863 .7216 .3059])
+                    plot(nonzeros(datafile(:,3)), 'LineStyle','-', 'LineWidth', 1.5, 'Marker', 'o','Color', [.0863 .7216 .3059], 'MarkerSize',10, 'MarkerFaceColor', [.0863 .7216 .3059])
+                    xlabel('Corrected f_0 values. x axis: glottal cycles')            
+                    ylabel('f_0 in Hz')
                     if ~isempty(numb)
                         disp(['Item that carries label ' num2str(numb(i)) '.'])
                     else
@@ -674,7 +773,9 @@ for i = 1:maxnb
                             datafile(cornb,10) = 0;
                             figure(1)
                             clf
-                            plot(datafile(:,1),datafile(:,10), 'LineStyle','--', 'Marker', 's','Color', [0.1490 0.7686 0.9255], 'MarkerSize',11, 'MarkerFaceColor', [0.1490 0.7686 0.9255])
+                            plot(datafile(:,10), 'LineStyle','--', 'Marker', '*','Color', 'black', 'MarkerSize',11, 'MarkerFaceColor', [0.1490 0.7686 0.9255])
+                            xlabel('Corrected O_q values. x axis: glottal cycles')            
+                            ylabel('O_q in %')
                         end
                     end
                 end
@@ -712,6 +813,19 @@ for i = 1:maxnb
     % saving the results in a temporary data file; can be recovered in case MatLab
     % suddenly closes (due to error, computer crash, power supply problem...)
     save tempdata data
+    
+    % Housekeeping: closing the figures. This matters for figure 4, which
+    % is only opened in a specific case (modifications to f0 values) and
+    % would thus remain open (and unchanged) when treating the next token.
+    figure(1)
+    close
+    figure(2)
+    close
+    figure(3)
+    close
+    figure(4)
+    close
+    
 % end of syllable loop
 end
 
